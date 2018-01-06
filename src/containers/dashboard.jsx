b3c78dea46c6a2f70bc246923d7b1e4529d83459
NamVH <hoainamak521996@gmail.com>
2018-01-06 22:21:20 +0700

    commit lai

--- src/containers/dashboard.jsx
+++ src/containers/dashboard.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import App from './app';
 // import CDashBoard from '../components/dashboard';
-// import CNTG from '../app/cntg/components';
+import CNTG from '../app/cntg/components';
 import Member from '../app/member/components/MemberList/MemberListContainer';
 import Main from '../app/Main/index';
 
 const DashBoard = () => {
   return (<div>
     <Main>
-      <Member />
-      {/*<CNTG />*/}
+      {/*<Member />*/}
+      <CNTG />
     </Main>
   </div>);
 };
